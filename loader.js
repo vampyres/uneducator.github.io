4514fbb457f03efd5e7a08e81c575121e4ede424
uneducator <125751752+uneducator@users.noreply.github.com>
2023-02-23 19:20:54 +0300

    Delete loader.js

--- loader.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Display the loading screen
-  document.getElementById("loading-screen").style.display = "none";
-
-  // Hide the loading screen after 1 second
-  setTimeout(function() {
-    document.getElementById("loading-screen").style.display = "none";
-  }, 1000);
-});
